589bdae725e2a9c16c837b4d762d90bac737bf05
AndrzejWojtani <awojtania@9bits.com.pl>
2017-05-03 21:02:09 +0200

    Processing Exception throwing

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -2,7 +2,6 @@ package wdsr.exercise3.hr;
 
 import wdsr.exercise3.ws.*;
 
-import javax.jws.WebParam;
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -14,69 +13,74 @@ import java.util.GregorianCalendar;
 // In order to see definition of the Human Resource web service:
 // 1. Run HolidayServerApp.
 // 2. Go to http://localhost:8090/holidayService/?wsdl
-public class HolidayClient{
+public class HolidayClient {
 
 	private HumanResourceService humanResourceService;
 
-    public HolidayClient(URL wsdlLocation) {
-    	humanResourceService = new HumanResourceService(wsdlLocation);
-    }
+	public HolidayClient(URL wsdlLocation) {
+		humanResourceService = new HumanResourceService(wsdlLocation);
+	}
 
-    /**
-     * Sends a holiday request to the HumanResourceService.
-     * 
-     * @param employeeId Employee ID
-     * @param firstName First name of employee
-     * @param lastName Last name of employee
-     * @param startDate First day of the requested holiday
-     * @param endDate Last day of the requested holiday
-     * @return Identifier of the request, if accepted.
-     * @throws ProcessingException if request processing fails.
-     */
-    public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-    	ObjectFactory holidayAndEmployeeTypeFactory=new ObjectFactory();
-    	HolidayResponse holidayResponse=new HolidayResponse();
-    	HolidayRequest holidayRequest=new HolidayRequest();
-    	
-    	EmployeeType employeeType=new EmployeeType();
-    	employeeType=holidayAndEmployeeTypeFactory.createEmployeeType();
-    	employeeType.setFirstName(firstName);
-    	employeeType.setLastName(lastName);
-    	employeeType.setNumber(employeeId);
-    	
-    	HolidayType holidayType=new HolidayType();
-    	holidayType=holidayAndEmployeeTypeFactory.createHolidayType();
-    	holidayType.setStartDate((dateToXMLGregorianCalendar(startDate)));
-    	holidayType.setEndDate((dateToXMLGregorianCalendar(endDate)));
-    	
-    	holidayRequest=holidayAndEmployeeTypeFactory.createHolidayRequest();
-    	holidayRequest.setEmployee(employeeType);
-    	holidayRequest.setHoliday(holidayType);
-    	
-    	HumanResource humanResourcePort=humanResourceService.getHumanResourcePort();
-    	
-		holidayResponse=humanResourcePort.holiday(holidayRequest);
-		
+	/**
+	 * Sends a holiday request to the HumanResourceService.
+	 * 
+	 * @param employeeId
+	 *            Employee ID
+	 * @param firstName
+	 *            First name of employee
+	 * @param lastName
+	 *            Last name of employee
+	 * @param startDate
+	 *            First day of the requested holiday
+	 * @param endDate
+	 *            Last day of the requested holiday
+	 * @return Identifier of the request, if accepted.
+	 * @throws ProcessingException
+	 *             if request processing fails.
+	 */
+	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate)
+			throws ProcessingException {
+		ObjectFactory holidayAndEmployeeTypeFactory = new ObjectFactory();
+		HolidayResponse holidayResponse = new HolidayResponse();
+		HumanResource humanResourcePort = humanResourceService.getHumanResourcePort();
+
+		EmployeeType employeeType = new EmployeeType();
+		employeeType = holidayAndEmployeeTypeFactory.createEmployeeType();
+		employeeType.setFirstName(firstName);
+		employeeType.setLastName(lastName);
+		employeeType.setNumber(employeeId);
+
+		HolidayType holidayType = new HolidayType();
+		holidayType = holidayAndEmployeeTypeFactory.createHolidayType();
+		holidayType.setStartDate((dateToXMLGregorianCalendar(startDate)));
+		holidayType.setEndDate((dateToXMLGregorianCalendar(endDate)));
+
+		HolidayRequest holidayRequest = new HolidayRequest();
+		holidayRequest = holidayAndEmployeeTypeFactory.createHolidayRequest();
+		holidayRequest.setEmployee(employeeType);
+		holidayRequest.setHoliday(holidayType);
+		try {
+			holidayResponse = humanResourcePort.holiday(holidayRequest);
+		} catch (Exception exception) {
+			throw new ProcessingException("Request processing failed");
+		}
+
 		return holidayResponse.getRequestId();
 	}
 
-   
-    public XMLGregorianCalendar dateToXMLGregorianCalendar(
-    		   Date date) 
-    		    {
-    		      DatatypeFactory df=null;
-				try {
-					df = DatatypeFactory.newInstance();
-				} catch (DatatypeConfigurationException e) {
-					e.printStackTrace();
-				}
-    		      if (date == null) {
-    		         return null;
-    		      } else {
-    		         GregorianCalendar gc = new GregorianCalendar();
-    		         gc.setTimeInMillis(date.getTime());
-    		         return df.newXMLGregorianCalendar(gc);
-    		      }
-    		   }
-
+	public XMLGregorianCalendar dateToXMLGregorianCalendar(Date date) {
+		DatatypeFactory df = null;
+		try {
+			df = DatatypeFactory.newInstance();
+		} catch (DatatypeConfigurationException e) {
+			e.printStackTrace();
+		}
+		if (date == null) {
+			return null;
+		} else {
+			GregorianCalendar gc = new GregorianCalendar();
+			gc.setTimeInMillis(date.getTime());
+			return df.newXMLGregorianCalendar(gc);
+		}
+	}
 }
