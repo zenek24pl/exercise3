cbe361e1ded3509bd6c94b8da1734dcf2686d9b8
AndrzejWojtani <awojtania@9bits.com.pl>
2017-05-03 18:26:16 +0200

    ObjectFactory,HolidayRequest,HolidayResponse,HolidayType and EmployeeType added to bookHoliday - problems with conversion/setStart/setEnd Date

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -2,6 +2,21 @@ package wdsr.exercise3.hr;
 
 import java.net.URL;
 import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+import wdsr.exercise3.hr.ws.*;
+import wdsr.exercise3.hr.ws.HolidayRequest;
+import wdsr.exercise3.hr.ws.HolidayResponse;
+import wdsr.exercise3.hr.ws.HolidayType;
+import wdsr.exercise3.hr.ws.HumanResourceService;
+
+
+
 
 // TODO Complete this class to book holidays by issuing a request to Human Resource web service.
 // In order to see definition of the Human Resource web service:
@@ -12,22 +27,67 @@ public class HolidayClient {
 	 * Creates this object
 	 * @param wsdlLocation URL of the Human Resource web service WSDL
 	 */
-	public HolidayClient(URL wsdlLocation) {
-		// TODO
+	private HumanResourceService humanResourceService;
+
+    public HolidayClient(URL wsdlLocation) {
+    	humanResourceService = new HumanResourceService(wsdlLocation);
+    }
+
+    /**
+     * Sends a holiday request to the HumanResourceService.
+     * 
+     * @param employeeId Employee ID
+     * @param firstName First name of employee
+     * @param lastName Last name of employee
+     * @param startDate First day of the requested holiday
+     * @param endDate Last day of the requested holiday
+     * @return Identifier of the request, if accepted.
+     * @throws ProcessingException if request processing fails.
+     */
+    public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
+    	ObjectFactory holidayAndEmployeeTypeFactory=new ObjectFactory();
+    	HolidayResponse holidayResponse=new HolidayResponse();
+    	HolidayRequest holidayRequest=new HolidayRequest();
+    	
+    	EmployeeType employeeType=new EmployeeType();
+    	employeeType=holidayAndEmployeeTypeFactory.createEmployeeType();
+    	employeeType.setFirstName(firstName);
+    	employeeType.setLastName(lastName);
+    	employeeType.setNumber(employeeId);
+    	
+    	HolidayType holidayType=new HolidayType();
+    	holidayType=holidayAndEmployeeTypeFactory.createHolidayType();
+    	holidayType.setStartDate((dateToXMLGregorianCalendar(startDate)));
+    	holidayType.setEndDate((dateToXMLGregorianCalendar(endDate)));
+    	
+    	holidayRequest=holidayAndEmployeeTypeFactory.createHolidayRequest();
+    	holidayRequest.setEmployee(employeeType);
+    	holidayRequest.setHoliday(holidayType);
+    	
+    	HumanResource humanResourcePort=humanResourceService.getHumanResourcePort();
+		
+    	holidayResponse=humanResourcePort.holiday(holidayRequest);
+		
+		return holidayResponse.getRequestId();
 	}
-	
-	/**
-	 * Sends a holiday request to the HumanResourceService.
-	 * @param employeeId Employee ID
-	 * @param firstName First name of employee
-	 * @param lastName Last name of employee
-	 * @param startDate First day of the requested holiday
-	 * @param endDate Last day of the requested holiday
-	 * @return Identifier of the request, if accepted.
-	 * @throws ProcessingException if request processing fails.
-	 */
-	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-		// TODO
-		return 0;
-	}
-}
+
+   
+    public XMLGregorianCalendar dateToXMLGregorianCalendar(
+    		   Date date) 
+    		    {
+    		      DatatypeFactory df=null;
+				try {
+					df = DatatypeFactory.newInstance();
+				} catch (DatatypeConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		      if (date == null) {
+    		         return null;
+    		      } else {
+    		         GregorianCalendar gc = new GregorianCalendar();
+    		         gc.setTimeInMillis(date.getTime());
+    		         return df.newXMLGregorianCalendar(gc);
+    		      }
+    		   }
+    }
---
+++ client/src/main/java/wdsr/exercise3/hr/ws/HolidayType.java
@@ -0,0 +1,94 @@
+
+package wdsr.exercise3.hr.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for HolidayType complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="HolidayType"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="StartDate" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *         &lt;element name="EndDate" type="{http://www.w3.org/2001/XMLSchema}date"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "HolidayType", propOrder = {
+    "startDate",
+    "endDate"
+})
+public class HolidayType {
+
+    @XmlElement(name = "StartDate", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar startDate;
+    @XmlElement(name = "EndDate", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar endDate;
+
+    /**
+     * Gets the value of the startDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * Sets the value of the startDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setStartDate(XMLGregorianCalendar value) {
+        this.startDate = value;
+    }
+
+    /**
+     * Gets the value of the endDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * Sets the value of the endDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setEndDate(XMLGregorianCalendar value) {
+        this.endDate = value;
+    }
+
+}
